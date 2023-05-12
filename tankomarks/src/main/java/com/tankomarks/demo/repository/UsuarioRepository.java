47e4db19bfb39791a265f06e940da99c27e53ebc
juanantoniovargas <128796454+juanantoniovargar@users.noreply.github.com>
2023-05-12 13:56:01 +0200

    rol solucionado, login funcional 100%

--- tankomarks/src/main/java/com/tankomarks/demo/repository/UsuarioRepository.java
+++ tankomarks/src/main/java/com/tankomarks/demo/repository/UsuarioRepository.java
@@ -1,16 +1,11 @@
 package com.tankomarks.demo.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-//import org.springframework.data.jpa.repository.Query;
 
-//import com.tankomarks.demo.model.Rol;
 import com.tankomarks.demo.model.Usuario;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
 	
 	public Usuario findByEmail(String email);
 	
-	// @Query(value = "SELECT nombre FROM rol where nombre = :nombre", nativeQuery = true)
-	// public Rol findByNombre(String nombre);
-
 }
--- tankomarks/src/main/java/com/tankomarks/demo/service/UsuarioServiceImpl.java
+++ tankomarks/src/main/java/com/tankomarks/demo/service/UsuarioServiceImpl.java
@@ -5,7 +5,7 @@ import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
-import java.util.ArrayList;
+//import java.util.ArrayList;
 
 import org.hibernate.exception.ConstraintViolationException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,24 +20,33 @@ import org.springframework.stereotype.Service;
 import com.tankomarks.demo.dto.RegistroDto;
 import com.tankomarks.demo.model.Usuario;
 import com.tankomarks.demo.model.Rol;
+import com.tankomarks.demo.repository.RolRepository;
 import com.tankomarks.demo.repository.UsuarioRepository;
 
 @Service
 public class UsuarioServiceImpl implements UsuarioService {
 	
 	private UsuarioRepository usuarioRepository;
+	
+	@Autowired
+	private RolRepository rolRepository;
 
 	@Autowired
 	private BCryptPasswordEncoder passwordEncoder;
-	
-	public UsuarioServiceImpl(UsuarioRepository usuarioRepository) {
+
+	@Autowired
+	public UsuarioServiceImpl(UsuarioRepository usuarioRepository, RolRepository rolRepository,
+			BCryptPasswordEncoder passwordEncoder) {
 		super();
 		this.usuarioRepository = usuarioRepository;
+		this.rolRepository = rolRepository;
+		this.passwordEncoder = passwordEncoder;
 	}
 
 	@Override
 	public Usuario guardar(RegistroDto registroDto) throws ConstraintViolationException {
 		
+		/*
 		Usuario usuario = new Usuario(			
 				registroDto.getNombre(),
 				registroDto.getEmail(),
@@ -45,6 +54,20 @@ public class UsuarioServiceImpl implements UsuarioService {
 				Arrays.asList(new Rol("USER")));
 
 		return usuarioRepository.save(usuario);
+		*/
+		
+		Rol userRole = rolRepository.findRolByNombre("USER");
+	    if (userRole == null) {
+	        userRole = new Rol("USER");
+	    }
+
+	    Usuario usuario = new Usuario(			
+	        registroDto.getNombre(),
+	        registroDto.getEmail(),
+	        passwordEncoder.encode(registroDto.getPassword()),
+	        Arrays.asList(userRole));
+
+	    return usuarioRepository.save(usuario);
 
 	}
 
