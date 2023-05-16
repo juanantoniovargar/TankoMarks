bcaf631fac43f502d466bd82950336b35bb40f58
juanantoniovargas <128796454+juanantoniovargar@users.noreply.github.com>
2023-05-16 14:55:56 +0200

    .

--- tankomarks/src/main/java/com/tankomarks/demo/controller/TankomarksController.java
+++ tankomarks/src/main/java/com/tankomarks/demo/controller/TankomarksController.java
@@ -1,6 +1,7 @@
 package com.tankomarks.demo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -12,7 +13,8 @@ public class TankomarksController {
 	}
 	
 	@GetMapping("/")
-	public String home() {
+	public String home(Model model) {
+		
 		return "index";
 	}
 
