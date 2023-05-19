31ca499d82622e3f5b0cce2a1a1e809378079a3b
juanantoniovargas <128796454+juanantoniovargar@users.noreply.github.com>
2023-05-19 14:45:17 +0200

    Busqueda 100% hay que arreglar footer que quede siempre abajo

--- tankomarks/src/main/java/com/tankomarks/demo/controller/TankomarksController.java
+++ tankomarks/src/main/java/com/tankomarks/demo/controller/TankomarksController.java
@@ -1,11 +1,21 @@
 package com.tankomarks.demo.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.tankomarks.demo.model.Manga;
+import com.tankomarks.demo.repository.MangaRepository;
+
 @Controller
 public class TankomarksController {
+	
+	@Autowired
+	private MangaRepository mangaRepo;
+	
 	
 	@GetMapping("/login")
 	public String login() {
@@ -13,9 +23,25 @@ public class TankomarksController {
 	}
 	
 	@GetMapping("/")
-	public String home(Model model) {
+	public String home(Model model, String busqueda) {
 		
-		return "index";
+		if (busqueda != null) {
+			
+			List<Manga> manga = mangaRepo.buscarMangas(busqueda); 
+			model.addAttribute("mangas", manga);
+			
+			int size = manga.size();
+			model.addAttribute("size", size);
+			
+			return "busqueda";
+			
+		} else {
+			
+			model.addAttribute("mangas", mangaRepo.mostrarMangas()); //provisional, solo quiero mostrar leyendos y leídos
+			return "index";
+			
+		}
+		
 	}
 	
 	@GetMapping("/favoritos")
---
+++ tankomarks/src/main/java/com/tankomarks/demo/repository/MangaRepository.java
@@ -0,0 +1,19 @@
+package com.tankomarks.demo.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import com.tankomarks.demo.model.Manga;
+
+public interface MangaRepository extends JpaRepository<Manga, Integer> {
+	
+	@Query(value="SELECT * FROM manga", nativeQuery=true)
+	List<Manga> mostrarMangas();
+	
+	@Query(value="SELECT * FROM manga WHERE nombre LIKE %:busqueda%", nativeQuery=true)
+	List<Manga> buscarMangas(@Param("busqueda") String busqueda);
+
+}
